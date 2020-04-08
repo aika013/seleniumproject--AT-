671a99aabf7b5c5061b534538520ce6541aacb5a
aika013 <58495031+aika013@users.noreply.github.com>
2020-04-07 23:15:24 -0400

    Delete HelloWorlPracticSession.java

--- src/test/java/com/cybertek/tests/day18_html_report/HelloWorlPracticSession.java
+++
@@ -1,7 +0,0 @@
-package com.cybertek.tests.day18_html_report;
-
-public class HelloWorlPracticSession {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
