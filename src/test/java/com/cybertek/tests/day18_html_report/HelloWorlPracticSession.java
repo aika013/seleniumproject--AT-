906ae3aafbbaddb76ef70ea2d3f33dd33918be7a
aika013 <58495031+aika013@users.noreply.github.com>
2020-04-07 23:13:58 -0400

    checking

---
+++ src/test/java/com/cybertek/tests/day18_html_report/HelloWorlPracticSession.java
@@ -0,0 +1,7 @@
+package com.cybertek.tests.day18_html_report;
+
+public class HelloWorlPracticSession {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
